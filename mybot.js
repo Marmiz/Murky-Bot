42c5cea8a37ed06e2b86b843122916dfad3cf085
Marmiz <claudio.restifo@gmail.com>
2016-10-04 13:45:35 +0700

    removed app methods

--- mybot.js
+++ mybot.js
@@ -5,9 +5,6 @@
 // import the discord.js module
 const Discord = require('discord.js');
 
-// import express modules
-var express = require('express');
-var app = express();
 
 // create an instance of a Discord Client, and call it bot
 const bot = new Discord.Client();
@@ -18,7 +15,7 @@ const token = process.env.TOKEN;
 // the ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted.
 bot.on('ready', () => {
-  console.log('I am ready in channel'+bot.channels.length+'!');
+  console.log('I am ready!');
 });
 
 // create an event listener for messages
@@ -31,15 +28,3 @@ bot.on('message', message => {
 
 // log our bot in
 bot.login(token);
-
-// heroku deployment
-// set port
-app.set('port', (process.env.PORT || 8080));
-
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function () {
-  console.log(`Server listening on port ${app.get('port')}`);
-});
