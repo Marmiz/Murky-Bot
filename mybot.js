ec5e7a0981b2d3bb42a592ef19515cfd610c2c06
Marmiz <claudio.restifo@gmail.com>
2016-10-04 12:54:34 +0700

    heroku

--- mybot.js
+++ mybot.js
@@ -18,7 +18,7 @@ const token = process.env.TOKEN;
 // the ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted.
 bot.on('ready', () => {
-  console.log('I am ready!');
+  console.log('I am ready in channel'+bot.channels.length+'!');
 });
 
 // create an event listener for messages
@@ -30,11 +30,15 @@ bot.on('message', message => {
 });
 
 // log our bot in
-bot.login(token);
+// bot.login(token);
 
 // heroku deployment
 // set port
 app.set('port', (process.env.PORT || 8080));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
 
 app.listen(app.get('port'), function () {
   console.log(`Server listening on port ${app.get('port')}`);
